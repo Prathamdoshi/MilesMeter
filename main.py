62c9888eb5b062bc85e99a37b440dccde047b964
Pratham Doshi <37569372+Prathamdoshi@users.noreply.github.com>
2023-09-05 10:12:09 -0700

    Created a Window Panel
    Created the window panel with a title, background color, and min size

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("This is working!")
+# import required libraries
+import tkinter
+
+# create the window
+window = tkinter.Tk() # instance
+window.title("MilesMeter") # application title
+window.config(background="white") # background color
+window.minsize(300,100) # height
+
+
+
+
+
+# run a constant while loop to keep the window listening
+window.mainloop()
